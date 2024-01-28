175a72578e4993cee12aafb7664d0e697ce643ad
AdelaNurSabila <126484291+AdelaNurSabila@users.noreply.github.com>
2024-01-28 11:40:53 +0700

    feat : event fixed and running

--- apps/web/src/app/create/page.js
+++ apps/web/src/app/create/page.js
@@ -2,6 +2,7 @@
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 import { useState } from 'react';
+import axios from 'axios';
 
 const Create = () => {
   const price_type_enum = { FREE: 'FREE', PAID: 'PAID' };
@@ -27,9 +28,12 @@ const Create = () => {
   const [available, setavailable] = useState('');
 
   const handleSubmit = async () => {
+   
+    // console.log(place)
+   
     try {
       const response = await axios.post(
-        'http://localhost:8000/event/createevent',
+        'http://localhost:8000/event/create-event',
         {
           tittle: tittle,
           description: description,
@@ -37,37 +41,40 @@ const Create = () => {
           date_event: date_event,
           price_type: price_type,
           price: price,
-          placel: place,
+          place: place,
           seats: seats,
           terms: terms,
           time: time,
+          category: category,
         },
       );
-
+    
       if (response.status === 201) {
         console.log(succses);
       } else {
         console.log(gagal);
       }
-    } catch (error) {}
+    } catch (error) {
+      console.log(error)
+    }
   };
   console.log;
   return (
   
 
-    <div className="bg-[#555486]    ">
+    <div className="bg-[#9c9cc0]">
         <Navbar/>
       <div className="bg-white p-10 md:w-3/4 lg:w-1/2 mx-auto rounded-xl mb-10 mt-10 ">
-        <h2 className="text-2xl font-bold ">Make Your Own Event</h2>
-        <p className="my-4 opacity-70">
+        <h2 className="text-2xl font-bold text-center ">Make Your Own Event</h2>
+        <p className="my-4 opacity-70 text-center">
           Adipisicing elit. Quibusdam magnam sed ipsam deleniti debitis
           laboriosam praesentium dolorum doloremque beata.
         </p>
         <hr className="my-6" />
-        <form action="">
+        <form action={handleSubmit}>
           <div className="flex items-center mb-5">
             <label
-              className="inline-block w-20 mr-6 text-right 
+              className="inline-block w-20 mr-6 text-left
                                              font-bold text-gray-600"
             >
               Tittle
@@ -83,9 +90,57 @@ const Create = () => {
                                   outline-none"
             />
           </div>
+
+          <div>
           <div className="flex items-center mb-5">
             <label
-              className="inline-block w-20 mr-6 text-right 
+              className=" w-20 mr-6 text-left 
+                                             font-bold text-gray-600"
+            >
+              Image
+            </label>
+              <div class="mt-1 flex justify-center px-6 pt-5 pb-6 w-[540px] border-2 border-gray-300 border-dashed rounded-md">
+                <div class="space-y-1 text-center">
+                  <svg
+                    class="mx-auto h-12 w-12 text-white"
+                    stroke="currentColor"
+                    fill="none"
+                    viewBox="0 0 48 48"
+                    aria-hidden="true"
+                  >
+                    <path
+                      d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+          <label
+          value={img}
+          onChange={(e) => setimg(e.target.value)}
+                      for="file-upload"
+                      class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                    >
+                      <span class="">Upload a file</span>
+                      <input
+                        id="file-upload"
+                        name="file-upload"
+                        type="file"
+                        class="sr-only"
+                      />
+                    </label>
+                    <p class="pl-1 text-white">or drag and drop</p>
+                    <p class="text-xs text-white">PNG, JPG, GIF up to 10MB</p>
+                    </div>
+                  </div>
+                </div>
+          </div> 
+          
+         
+
+          <div className="flex items-center mb-5">
+            <label
+              className="inline-block w-20 mr-6 text-left 
                                              font-bold text-gray-600"
             >
               Category
@@ -105,7 +160,7 @@ const Create = () => {
 
           <div className="flex items-center mb-5">
             <label
-              className="inline-block w-20 mr-6 text-right 
+              className="inline-block w-20 mr-6 text-left 
                                              font-bold text-gray-600"
             >
               Date
@@ -123,7 +178,7 @@ const Create = () => {
           </div>
           <div className="flex items-center mb-5">
             <label
-              className="inline-block w-20 mr-6 text-right 
+              className="inline-block w-20 mr-6 text-left 
                                              font-bold text-gray-600"
             >
               Location
@@ -142,7 +197,7 @@ const Create = () => {
 
           <div className="flex items-center mb-5">
             <label
-              className="inline-block w-20 mr-6 text-right 
+              className="inline-block w-20 mr-6 text-left 
                                              font-bold text-gray-600"
             >
               Description
@@ -161,7 +216,7 @@ const Create = () => {
 
           <div className="flex items-center mb-5">
             <label
-              className="inline-block w-20 mr-6 text-right 
+              className="inline-block w-20 mr-6 text-left 
                                              font-bold text-gray-600"
             >
               Terms
@@ -180,7 +235,7 @@ const Create = () => {
 
           <div className="flex items-center mb-5">
             <label
-              className="inline-block w-20 mr-6 text-right 
+              className="inline-block w-20 mr-6 text-left 
                                              font-bold text-gray-600"
             >
               Seats
@@ -199,7 +254,7 @@ const Create = () => {
 
           <div className="flex items-center mb-5">
             <label
-              className="inline-block w-20 mr-6 text-right 
+              className="inline-block w-20 mr-6 text-left 
                                              font-bold text-gray-600"
             >
               Time
@@ -217,7 +272,7 @@ const Create = () => {
           </div>
           <div className="flex items-center mb-5">
             <label
-              className="inline-block w-20 mr-6 text-right 
+              className="inline-block w-20 mr-6 text-left 
                                      font-bold text-gray-600"
             >
               Ticket Category
@@ -237,7 +292,7 @@ const Create = () => {
           </div>
           <div className="flex items-center mb-5">
             <label
-              className="inline-block w-20 mr-6 text-right 
+              className="inline-block w-20 mr-6 text-left 
                                              font-bold text-gray-600"
             >
               Price
@@ -255,7 +310,7 @@ const Create = () => {
           </div>
 
           <div className="text-right">
-            <button className="py-3 px-8 bg-green-400 text-white font-bold">
+            <button type='submit' className="py-3 px-8 bg-[#555486] hover:bg-blue-500 text-white font-bold">
               Submit
             </button>
           </div>
